be5c7154c06a9cd1ecdeddbb1e847f5ba54dd0a3
kareemhanafi <85454273+kareemhanafi@users.noreply.github.com>
2021-06-06 20:56:13 +0200

    Add files via upload

---
+++ distance.c
@@ -0,0 +1,12 @@
+double distance(double lat1, double lat2, double lon1, double lon2) 
+    {  int R = 6371; // Radius of the earth
+
+    double latDistance = Math.toRadians(lat2 - lat1);
+    double lonDistance = Math.toRadians(lon2 - lon1);
+    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double distance = R * c * 1000; // convert to meters
+    return distance;
+	}
